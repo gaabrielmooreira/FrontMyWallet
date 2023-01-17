36f4ab95e3e1e3ffa4e87cd467029559de8de1cd
gaabrielmooreira <gaabrielmooreira@gmail.com>
2023-01-17 17:17:58 -0300

    Revert "LoginPage Layout"
    This reverts commit a17acad4334d16473c5266119616df7ae28294d7.

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,27 +1,7 @@
-
-import styled from "styled-components"
-import { StyledForm, StyledLink, StyledLogo } from "../styles/Styled"
-
-export default function LoginPage(){
+export default function RegisterPage(){
     return (
-        <StyledLoginPage>
-            <StyledLogo>MyWallet</StyledLogo>
-            <StyledForm>
-                <input type="email" placeholder="E-mail" required/>
-                <input type="password" placeholder="Senha" required/>
-                <button>Entrar</button>
-            </StyledForm>
-            <StyledLink to="/cadastro">Primeira vez? Cadastre-se!</StyledLink>
-        </StyledLoginPage>
+        <div>
+            RegisterPage
+        </div>
     )
 }
-
-const StyledLoginPage = styled.div`
-    padding: 0px 25px;
-    height: 100vh;
-    background-color: #8C11BE;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-`
