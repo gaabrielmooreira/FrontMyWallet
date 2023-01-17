2db62aa4d4d23c3873756228afd58bd71af37411
gaabrielmooreira <gaabrielmooreira@gmail.com>
2023-01-17 16:54:59 -0300

    NewEntryPage Layout

--- src/pages/NewEntryPage.js
+++ src/pages/NewEntryPage.js
@@ -1,7 +1,14 @@
+import { StyledForm, StyledH2, StyledTransferPages } from "../styles/Styled"
+
 export default function NewEntryPage(){
     return (
-        <div>
-            NewEntryPage
-        </div>
+        <StyledTransferPages>
+            <StyledH2>Nova entrada</StyledH2>
+            <StyledForm>
+                <input type="number" placeholder="Valor"/>
+                <input type="tex" placeholder="Descrição"/>
+                <button>Salvar entrada</button>
+            </StyledForm>
+        </StyledTransferPages>
     )
 }
