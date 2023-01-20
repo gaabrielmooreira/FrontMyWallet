3ebc0ea781439a2e1f2924bbd15558886e49285c
gaabrielmooreira <gaabrielmooreira@gmail.com>
2023-01-20 15:04:37 -0300

    function saveExit

--- src/components/FormNewExit.js
+++ src/components/FormNewExit.js
@@ -1,10 +1,36 @@
 import { StyledForm } from "../styles/Styled.js"
+import { useContext } from "react";
+import AuthContext from "../contexts/AuthContext.js";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 export default function FormNewExit() {
+    const [value, setValue] = useState("");
+    const [description, setDescription] = useState("");
+
+    const navigate = useNavigate();
+
+    const { token } = useContext(AuthContext);
+
+    async function saveExit(event) {
+        event.preventDefault();
+        const headers = {
+            Authorization: `Bearer ${token}`
+        }
+        const body = { value, description };
+        try {
+            await axios.post(`${process.env.REACT_APP_API_URL}/nova-entrada`, body, headers);
+        } catch (err) {
+            return console.log(err);
+        }
+        navigate("/home");
+    }
+
     return (
-        <StyledForm>
-            <input type="number" placeholder="Valor" />
-            <input type="tex" placeholder="Descrição" />
-            <button>Salvar saída</button>
+        <StyledForm onSubmit={saveExit}>
+            <input value={value} onChange={(e) => setValue(e.target.value)} type="text" placeholder="Valor" />
+            <input value={description} onChange={(e) => setDescription(e.target.value)} type="text" placeholder="Descrição" />
+            <button type="submit">Salvar saída</button>
         </StyledForm>
     )
 }
